4253da998ea35abb3d2427e69ee366df447b9384
Bobby McMillen <bobbym@virtualofficepro.com>
2022-04-06 10:03:24 -0500

    init

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const button = document.getElementById('menu-btn')
+const navi = document.getElementById('menu')
+
+button.addEventListener('click', () => {
+    button.classList.toggle('open')
+    navi.classList.toggle('flex')
+    navi.classList.toggle('hidden')
+})
