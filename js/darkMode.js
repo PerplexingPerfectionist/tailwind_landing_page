763021e6a7ed3f9eba10b41f4407f583c0cda77d
Bobby McMillen <bobbym@virtualofficepro.com>
2022-04-12 11:00:19 -0500

    Careers Page and Accordion JS

--- js/darkMode.js
+++ js/darkMode.js
@@ -4,10 +4,18 @@ const dayNight = document.getElementById('dayNight')
 const sun = document.getElementById('sun')
 const moon = document.getElementById('moon')
 
-dayNight.addEventListener('click', () => {
+function handleDarkMode() {
     darkMode.classList.toggle('dark')
     sun.classList.toggle('hidden')
     sun.classList.toggle('flex')
     moon.classList.toggle('flex')
     moon.classList.toggle('hidden')
-})
+}
+
+dayNight.addEventListener('click', handleDarkMode)
+
+window.onunload = function() {
+    console.log("clearing event listeners prior to leaving page");
+    document.body.removeEventListener('click', handleDarkMode);
+    return;
+}
--- js/script.js
+++ js/script.js
@@ -2,8 +2,16 @@
 const button = document.getElementById('menu-btn')
 const navi = document.getElementById('menu')
 
-button.addEventListener('click', () => {
+function handleMenuClick() {
     button.classList.toggle('open')
     navi.classList.toggle('flex')
     navi.classList.toggle('hidden')
-})
+}
+
+button.addEventListener('click', handleMenuClick)
+
+window.onunload = function() {
+    console.log("clearing event listeners prior to leaving page");
+    document.body.removeEventListener('click', handleMenuClick);
+    return;
+}
